93a485217e84de4bbd1e981df2e460946b1f046d
sylvainbonnot <sppbonnot@gmail.com>
2022-07-18 12:40:24 +0200

    audio_histo

--- plunk/sb/front_experiments/audio_histo.py
+++ plunk/sb/front_experiments/audio_histo.py
@@ -1,34 +1,55 @@
 import threading
-
+import av
 import cv2
 import streamlit as st
 from matplotlib import pyplot as plt
 
 from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import (
+    AudioProcessorBase,
+    ClientSettings,
+    WebRtcMode,
+    webrtc_streamer,
+)
+from streamlit_webrtc import (
+    RTCConfiguration,
+    WebRtcMode,
+    WebRtcStreamerContext,
+    webrtc_streamer,
+)
 
-lock = threading.Lock()
-audio_container = {"audio": None}
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
 
 
-def audio_frame_callback(frame):
-    audio = frame.to_ndarray(format="bgr24")
-    with lock:
-        audio_container["audio"] = audio
+def process_audio(frame: av.AudioFrame) -> av.AudioFrame:
+    raw_samples = frame.to_ndarray()
+    # sound = pydub.AudioSegment(
+    #     data=raw_samples.tobytes(),
+    #     sample_width=frame.format.bytes,
+    #     frame_rate=frame.sample_rate,
+    #     channels=len(frame.layout.channels),
+    # )
+    st.write(raw_samples)
+    # sound = sound.apply_gain(gain)
 
-    return frame
+    # Ref: https://github.com/jiaaro/pydub/blob/master/API.markdown#audiosegmentget_array_of_samples  # noqa
+    # channel_sounds = sound.split_to_mono()
+    # channel_samples = [s.get_array_of_samples() for s in channel_sounds]
+    # new_samples: np.ndarray = np.array(channel_samples).T
+    # new_samples = new_samples.reshape(raw_samples.shape)
+
+    # new_frame = av.AudioFrame.from_ndarray(new_samples, layout=frame.layout.name)
+    # new_frame.sample_rate = frame.sample_rate
+    return raw_samples
 
 
-ctx = webrtc_streamer(key="example", audio_frame_callback=audio_frame_callback)
-
-fig_place = st.empty()
-fig, ax = plt.subplots(1, 1)
-
-while ctx.state.playing:
-    with lock:
-        audio = audio_container["audio"]
-    if audio is None:
-        continue
-    gray = cv2.cvtColor(audio, cv2.COLOR_BGR2GRAY)
-    ax.cla()
-    ax.hist(gray.ravel(), 256, [0, 256])
-    fig_place.pyplot(fig)
+webrtc_streamer(
+    key="audio-filter",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration=RTC_CONFIGURATION,
+    audio_frame_callback=process_audio,
+    async_processing=True,
+)
