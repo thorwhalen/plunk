3e2dce196dd83cc8a69eae29d80892082cc59537
sylvainbonnot <sppbonnot@gmail.com>
2022-07-20 17:02:24 +0200

    chk_if_click

--- plunk/sb/front_experiments/edge_interface/chk_if_click.py
+++ plunk/sb/front_experiments/edge_interface/chk_if_click.py
@@ -1,9 +1,19 @@
+from hear import WavSerializationTrans, WavLocalFileStore
+from hum import disp_wf, plot_wf
+
+my_wav_store = WavLocalFileStore("./data/ClickDetection")
+click_template = my_wav_store["base_click.wav"]
+wf_base = my_wav_store["wf_base.wav"]
+
+print(len(wf_base))
+
+
 if __name__ == "__main__":
     from hear import WavSerializationTrans, WavLocalFileStore
     from hum import disp_wf, plot_wf
 
-    my_wav_store = WavLocalFileStore("/data/ClickDetection")
+    my_wav_store = WavLocalFileStore("./data/ClickDetection")
     click_template = my_wav_store["base_click.wav"]
     wf_base = my_wav_store["wf_base.wav"]
 
-    disp_wf(wf_base)
+    print(len(wf_base))
