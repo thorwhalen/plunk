4c03d307a930f0e9d1506baf78c7a050b5fc3137
sylvainbonnot <sppbonnot@gmail.com>
2022-07-20 20:25:15 +0200

    add river examples

--- plunk/sb/front_experiments/edge_interface/chk_if_click.py
+++ plunk/sb/front_experiments/edge_interface/chk_if_click.py
@@ -1,19 +1,17 @@
 from hear import WavSerializationTrans, WavLocalFileStore
-from hum import disp_wf, plot_wf
+from forged.clickify import clickify, pad_to_length
 
+# data prep
 my_wav_store = WavLocalFileStore("./data/ClickDetection")
 click_template = my_wav_store["base_click.wav"]
 wf_base = my_wav_store["wf_base.wav"]
+long_click = pad_to_length(click_template, len(wf_base))
 
-print(len(wf_base))
+# place clicks at arbitrary timestamps
+timestamps = [2000, 10000, 20000, 40000, 60000]
+wf_with_clicks = clickify(wf_base, timestamps, long_click, click_marker=None)
 
 
 if __name__ == "__main__":
-    from hear import WavSerializationTrans, WavLocalFileStore
-    from hum import disp_wf, plot_wf
 
-    my_wav_store = WavLocalFileStore("./data/ClickDetection")
-    click_template = my_wav_store["base_click.wav"]
-    wf_base = my_wav_store["wf_base.wav"]
-
-    print(len(wf_base))
+    print(len(wf_with_clicks))
