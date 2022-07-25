f8d999783187e034aba425ec1c905fb8e136197e
sylvainbonnot <sppbonnot@gmail.com>
2022-07-25 16:59:25 +0200

    update

--- plunk/sb/odat_lab/guns.py
+++ plunk/sb/odat_lab/guns.py
@@ -1,10 +1,7 @@
 from py2store import myconfigs
-from py2store.stores.local_store import RelativeDirPathFormatKeys, PickleStore
-from hear.session_block_stores import BlockWfStore, ScoopAnnotationsStore
 import numpy as np
 from functools import partial
-from sklearn.preprocessing import normalize, RobustScaler
-from graze import graze
+from sklearn.preprocessing import normalize
 from hear import WavLocalFileStore
 from dol import wrap_kvs
 import soundfile as sf
@@ -32,23 +29,21 @@ def WfStore(root_store):
 
 class Dacc:
     def __init__(self, root_dir=DFLT_LOCAL_SOURCE_DIR):
-        self.root_store = WavLocalFileStore(root_dir)
-        self.wfs = WfStore(self.root_store)
+        self.wfs = WavLocalFileStore(root_dir)
 
+    def wf_tag_gen(self):
+        for key in self.wfs:
+            signal = self.wfs[key]
+            normal_wf = normalize(np.float32(signal).reshape(1, -1))[0]
 
-#     def wf_tag_gen(self):
-#         for annot in self.annots:
-#             bt, tt, tag = annot['bt'], annot['tt'], annot['tag']
-#             for wf in self.block_store.block_search(bt=bt, tt=tt).values():
-#                 normal_wf = normalize(np.float32(wf).reshape(1, -1))[0]
-#                 yield normal_wf, tag
-#                 # yield wf, tag
+            yield normal_wf, key
 
-#     def chk_tag_gen(self, chunker=DFLT_CHUNKER):
-#         for wf, tag in self.wf_tag_gen():
-#             for chk in chunker(wf):
-#                 yield chk, tag
+    def chk_tag_gen(self, chunker=DFLT_CHUNKER):
+        for wf, tag in self.wf_tag_gen():
+            for chk in chunker(wf):
+                yield chk, tag
+
 
 if __name__ == "__main__":
     dacc = mk_dacc()
-    print(list(dacc.wfs.keys())[:5])
+    print(next(dacc.chk_tag_gen()))
