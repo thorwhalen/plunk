929ae18c643ef26ea42520c6b6ccb61d9b77519d
sylvainbonnot <sppbonnot@gmail.com>
2022-07-29 18:49:44 +0200

    sound_tagger2

--- plunk/sb/front_experiments/edge_interface/edge_app/pages/display.py
+++ plunk/sb/front_experiments/edge_interface/edge_app/pages/display.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.title("Display")
 st.write(st.session_state["shared"])
+st.session_state["shared"] = "page display visited"
--- plunk/sb/front_experiments/edge_interface/sound_tagger.py
+++ plunk/sb/front_experiments/edge_interface/sound_tagger.py
@@ -1,9 +1,9 @@
 from typing import Iterable
 from meshed import code_to_dag, DAG
 from front import APP_KEY, RENDERING_KEY, ELEMENT_KEY, NAME_KEY, OBJ_KEY
-
+from collections.abc import Callable
 from front.crude import prepare_for_crude_dispatch
-from streamlitfront.elements import TextInput
+from streamlitfront.elements import TextInput, SelectBox
 
 from streamlitfront.base import mk_app
 from streamlitfront.examples.util import Graph
@@ -14,13 +14,23 @@ from streamlitfront.elements import (
 )
 import streamlit as st
 
-param_to_mall_map = dict(train_audio="train_audio", tag="tag_store")
+param_to_mall_maps = dict(train_audio="train_audio", tag="tag_store")
 
-mall = dict(
-    train_audio={},
-    tag_store={},
-    # unused_store={"to": "illustrate"},
-)
+if "mall" not in st.session_state:
+    st.session_state["mall"] = dict(
+        # train_audio={},
+        # tag={},
+        # unused_store={"to": "illustrate"},
+        tag_sound_output={}
+    )
+
+mall = st.session_state["mall"]
+# mall = dict(
+#     # train_audio={},
+#     # tag={},
+#     # unused_store={"to": "illustrate"},
+#     tag_sound_output={}
+# )
 
 
 def crudify(funcs):
@@ -33,17 +43,35 @@ def crudify(funcs):
 WaveForm = Iterable[int]
 
 
-@code_to_dag
-def tag_sound(train_audio: WaveForm, tag):
+# @code_to_dag
+@prepare_for_crude_dispatch(mall=mall, output_store="tag_sound_output")
+def tag_sound(train_audio: WaveForm, tag: str):
     # mall["tag_store"] = tag
-    result = tag_sound(train_audio, tag)
+    return (train_audio, tag)
 
+
+@prepare_for_crude_dispatch(mall=mall, param_to_mall_map={"result": "tag_sound_output"})
+def display_tag_sound(result):
+    return result
+
+
+# crudified_tag_sound = prepare_for_crude_dispatch(
+#     tag_sound, mall=mall, output_store="tag_sound_output"
+# )
+# crudified_display_tag_sound = prepare_for_crude_dispatch(
+#     display_tag_sound, mall=mall, param_to_mall_map={"result": "tag_sound_output"}
+# )
+
+print(type(tag_sound))
+from i2 import Sig
+
+print(Sig(display_tag_sound))
 
 config_ = {
     APP_KEY: {"title": "Simple Real Audio ML"},
     # OBJ_KEY: {"trans": crudify},
     RENDERING_KEY: {
-        DAG: {
+        "tag_sound": {
             # "description": {"content": "A very simple learn model example."},
             "execution": {
                 "inputs": {
@@ -55,19 +83,41 @@ config_ = {
                         },
                         "From the microphone": {ELEMENT_KEY: AudioRecorder},
                     },
-                    "tag_store": {
-                        ELEMENT_KEY: TextInput,
-                    },
+                    # "tag": {
+                    #     ELEMENT_KEY: TextInput,
+                    # },
                 },
+            }
+        },
+        "display_tag_sound": {
+            "execution": {
+                "inputs": {
+                    "result": {
+                        ELEMENT_KEY: SelectBox,
+                        "options": mall["tag_sound_output"],
+                    },
+                }
             },
+        },
+        DAG: {
             "graph": {
                 ELEMENT_KEY: Graph,
                 NAME_KEY: "Flow",
             },
-        }
+        },
+        Callable: {
+            "execution": {
+                "inputs": {
+                    "save_name": {
+                        ELEMENT_KEY: TextInput,
+                        NAME_KEY: "Save output as",
+                    },
+                }
+            },
+        },
     },
 }
 
-app = mk_app([tag_sound], config=config_)
+app = mk_app([tag_sound, display_tag_sound], config=config_)
 app()
 st.write(mall)
