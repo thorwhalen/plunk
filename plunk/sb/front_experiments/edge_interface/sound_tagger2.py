ddc2fd7af59d8107a627aedf393bd459a90d7602
sylvainbonnot <sppbonnot@gmail.com>
2022-07-29 17:07:12 +0200

    edge_app

---
+++ plunk/sb/front_experiments/edge_interface/sound_tagger2.py
@@ -0,0 +1,11 @@
+from streamlitfront.elements import (
+    AudioRecorder,
+    FileUploader,
+    MultiSourceInputContainer,
+)
+import streamlit as st
+
+st.title("Loader")
+
+
+s = AudioRecorder()
