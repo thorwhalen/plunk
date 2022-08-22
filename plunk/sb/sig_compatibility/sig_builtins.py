e7e7ed42c69158a07b6b2c3f5d9b84fa68739279
sylvainbonnot <sppbonnot@gmail.com>
2022-08-22 18:11:40 +0200

    create sig_builtins

---
+++ plunk/sb/sig_compatibility/sig_builtins.py
@@ -0,0 +1,51 @@
+import builtins
+import types
+
+
+def get_builtin_function_types():
+    builtin_function_names = [
+        name
+        for name, obj in vars(builtins).items()
+        if isinstance(obj, types.BuiltinFunctionType)
+    ]
+    return builtin_function_names
+
+
+def get_lines_from_string(text):
+    return text.split("\n")
+
+
+def extract_doc(doc):
+    lines = get_lines_from_string(doc)
+    result = []
+    for line in lines:
+        if line.strip():
+            result.append(line)
+        else:
+            break
+    return result
+
+
+def enclosed_text(text):
+    idx1 = text.find("(")
+    idx2 = text.find(")", idx1)
+
+    if idx1 != -1 and idx2 != -1 and (idx2 > idx1 + 1):
+        result = text[idx1 + 1 : idx2]
+    return result
+
+
+def args_for_builtin(builtin_func):
+    """Extract the args from builtin doc"""
+
+    docstr = builtin_func.__doc__
+    doc = extract_doc(docstr)
+    name = builtin_func.__name__
+
+    result = []
+    for line in doc:
+        s = line.replace(name, "", 1)
+        args = enclosed_text(s)
+        result.append(args)
+
+    return result
