23296119a18991e4fb5ebba81e9bea8f415aa5cc
sylvainbonnot <sppbonnot@gmail.com>
2022-09-08 17:16:22 +0200

    add async example

---
+++ plunk/sb/front_experiments/sensor_stream/get_stream3.py
@@ -0,0 +1,18 @@
+import asyncio
+from websockets import connect
+import datetime
+from streamz import Stream
+
+source = Stream()
+source.sink(print)
+
+
+async def accelerometer(uri):
+    async with connect(uri) as websocket:
+        while True:
+            data = await websocket.recv()
+            source.emit(data)
+
+
+URI = "ws://192.168.1.3:8080/sensor/connect?type=android.sensor.accelerometer"
+asyncio.run(accelerometer(URI))
