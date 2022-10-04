06d979ce4d0edd7585903818d49e498fc9909e6d
sylvainbonnot <sppbonnot@gmail.com>
2022-10-04 16:00:49 +0200

    add data_prep2

---
+++ plunk/sb/front_experiments/streamlitfront_dataprep/analysis.py
@@ -0,0 +1,71 @@
+import numpy as np
+from creek.tools import apply_func_to_index, fanout_and_flatten
+import umap
+import umap.plot
+from functools import partial
+from hear import WavLocalFileStore
+
+
+chunker = DFLT_CHUNKER
+featurizer = DFLT_FEATURIZER
+
+
+def store_to_key_fvs(wf_store, chunker=DFLT_CHUNKER, featurizer=DFLT_FEATURIZER):
+    wf_items = wf_store.items()
+    key_chk_tuples = fanout_and_flatten(wf_items, chunker, 1)
+    featurizer_iter = partial(apply_func_to_index, apply_to_idx=1, func=featurizer)
+    yield from map(featurizer_iter, key_chk_tuples)
+
+
+def key_fvs_to_tag_fvs(key_fvs, annots_df):
+    func = partial(key_to_tag_from_annots, annots_df=annots_df)
+    tagger = partial(apply_func_to_index, apply_to_idx=0, func=func)
+
+    yield from map(tagger, key_fvs)
+
+
+def key_to_tag_from_annots(key, annots_df):
+    tag = annots_df["tag"][annots_df["key"] == key].values[0]
+    return tag
+
+
+def mk_Xy(tag_fv_iterator):
+    y, X = zip(*list(tag_fv_iterator))
+    return np.array(X), y
+
+
+def preprocess(X_train):
+
+    from shaded.chained_spectral_projector import learn_chain_proj_matrix
+
+    proj_matrix = learn_chain_proj_matrix(X_train)
+    X_train_proj = np.dot(X_train, proj_matrix)
+
+    return proj_matrix, X_train_proj
+
+
+def tag_from_key(key):
+    match = re.match(r"(\w*)\/(\w*)", key)
+
+    tag = match.group(2)
+    return tag
+
+
+def train_test_from_key(key):
+    return key.split("/")[0]
+
+
+def plot_umap(Xy, show_legend=True):
+    X, y = Xy
+    mapper = umap.UMAP().fit(X)
+    umap.plot.points(mapper, labels=np.array(y), show_legend=show_legend)
+
+
+def load_dataset(folder_path, annot_path):
+    wf_store = WavLocalFileStore(folder_path)
+    df = pd.read_csv(annot_path)
+    key_fvs = store_to_key_fvs(wf_store)
+    tag_fv_iterator = key_fvs_to_tag_fvs(key_fvs, annots_df=df)
+    X, y = mk_Xy(tag_fv_iterator)  # Cmd+]
+
+    return X, y
---
+++ plunk/sb/front_experiments/streamlitfront_dataprep/data_prep2.py
@@ -0,0 +1,122 @@
+from dataclasses import dataclass
+from functools import partial
+from io import BytesIO
+import os
+from pathlib import Path
+import time
+from typing import Any, Iterable, Union
+from dol import Files
+from front.elements import DEFAULT_INPUT_KEY, OutputBase
+from meshed import code_to_dag, DAG
+from front import APP_KEY, RENDERING_KEY, ELEMENT_KEY, NAME_KEY, OBJ_KEY
+from collections.abc import Callable
+from front.crude import Crudifier, prepare_for_crude_dispatch
+from streamlitfront.elements import TextInput, SelectBox
+from dol.appendable import appendable
+import soundfile as sf
+import matplotlib.pyplot as plt
+from streamlit.uploaded_file_manager import UploadedFile
+
+from streamlitfront import mk_app, binder as b
+from streamlitfront.examples.util import Graph
+from streamlitfront.elements import (
+    AudioRecorder,
+    FileUploader,
+    MultiSourceInput,
+)
+import streamlit as st
+
+import os
+import soundfile as sf
+import pandas as pd
+import zipfile
+from io import BytesIO
+from pathlib import Path
+
+from dol.appendable import add_append_functionality_to_store_cls
+from dol import Store
+from dol import FilesOfZip, wrap_kvs, filt_iter
+
+from py2store import FilesOfZip
+from hear import WavLocalFileStore
+from dol import FuncReader
+
+
+# ============ BACKEND ============
+WaveForm = Any
+DFLT_WF_PATH = "/Users/sylvain/Dropbox/Otosense/VacuumEdgeImpulse/"
+DFLT_ANNOT_PATH = "/Users/sylvain/Dropbox/sipyb/Testing/data/annots_vacuum.csv"
+
+
+from hear import WavLocalFileStore
+from dol import FuncReader
+
+
+def my_obj_of_data(b):
+    return sf.read(BytesIO(b), dtype="float32")[0]
+
+
+@wrap_kvs(obj_of_data=my_obj_of_data)
+@filt_iter(filt=lambda x: not x.startswith("__MACOSX") and x.endswith(".wav"))
+class WfZipStore(FilesOfZip):
+    """Waveform access. Keys are .wav filenames and values are numpy arrays of int16 waveform."""
+
+    pass
+
+
+def key_to_ext(k):
+    _, ext = os.path.splitext(k)
+    if ext.startswith("."):
+        ext = ext[1:]
+    return ext
+
+
+def processor_from_ext(ext):
+    if ext.startswith("."):
+        ext = ext[1:]
+    if ext in {"zip"}:
+        pass
+    elif ext in {"wav"}:
+        pass
+
+
+def is_zip_file(filepath):
+    return zipfile.is_zipfile(filepath)
+
+
+def is_dir(filepath):
+    return os.path.isdir(filepath)
+
+
+def key_maker(name, prefix):
+    return f"{prefix}_{name}"
+
+
+def wf_store_factory(filepath):
+    key = key_maker(name=filepath, prefix="wf_store")
+    tag = "wf_store"
+
+    if is_dir(filepath):
+        data = WavLocalFileStore(filepath)
+
+    elif is_zip_file(filepath):
+        data = WfZipStore(filepath)
+
+    return mk_store_item(key, tag, data)
+
+
+def annot_store_factory(filepath):
+    key = key_maker(name=filepath, prefix="annot_store")
+    tag = "annot_store"
+
+    data = pd.read_csv(filepath)
+
+    return mk_store_item(key, tag, data)
+
+
+def mk_store_item(key, tag, data):
+    return dict(key=key, tag=tag, data=data)
+
+
+if __name__ == "__main__":
+    pass
