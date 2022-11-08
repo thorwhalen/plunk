48239a3757eba5ac89efefe5f5b67158ff0e5f6d
Andie.Phan <Andie.Phan@analog.com>
2022-11-07 22:12:31 -0800

    StreamViz LiveGraph data buffered with SlabsSourceReader

--- plunk/ap/live_graph/live_graph.py
+++ plunk/ap/live_graph/live_graph.py
@@ -4,13 +4,16 @@
 [ ] Add to webservice.
 [ ] Visualize graph.
 """
-from time import sleep
+from time import sleep, time
+from typing import Any, Callable
 
 import numpy as np
 from audiostream2py import PyAudioSourceReader, get_input_device_index
 from know.base import SlabsIter, IteratorExit
 from know.examples.keyboard_and_audio import only_if
 from recode import mk_codec
+from stream2py import SourceReader
+from stream2py.utility.typing_hints import ComparableType
 
 codec = mk_codec('h')
 
@@ -79,20 +82,58 @@ def audio_it(
         wf=audio_to_wf,
         audio_reader_instance=lambda: audio_buffer,
         _audio_stop=stop_if_audio_not_running,
-        **{k: v for k, v in GRAPH_TYPES.items() if k in graph_types}
+        **{k: v for k, v in GRAPH_TYPES.items() if k in graph_types},
     )
+
+
+class SlabsSourceReader(SourceReader):
+    def __init__(self, slabs_it: SlabsIter, key: Callable, skip: Callable = None):
+        self.slabs_it = slabs_it
+        self.key_func = key
+        self.skip_func = skip
+
+    def open(self) -> None:
+        self.slabs_it.open()
+
+    def close(self) -> None:
+        self.slabs_it.close()
+
+    def key(self, data: Any) -> ComparableType:
+        return self.key_func(data)
+
+    def read(self) -> dict:
+        if self.skip_func(data := next(self.slabs_it)):
+            return None
+        return data
 
 
 if __name__ == '__main__':
     print(PyAudioSourceReader.list_recording_devices())
-    with audio_it(input_device='NexiGo N930AF FHD Webcam Audio') as a_it:
+
+    def get_timestamp(data):
+        return data.get('audio')[0]
+
+    def skip_data(data) -> bool:
+        return data.get('audio') is None
+
+    start = time() * 1e6
+    with SlabsSourceReader(
+        slabs_it=audio_it(input_device='NexiGo N930AF FHD Webcam Audio'),
+        key=get_timestamp,
+        skip=skip_data,
+    ).stream_buffer(100) as slab_buffer:
         i = 0
-        for slab in a_it:
-            if slab.get('audio') is not None:
-                print(slab.get('volume'))
+
+        slab_reader = slab_buffer.mk_reader()
+        while slab_buffer.is_running:
+            if (slab := slab_reader.next(ignore_no_item_found=True)) is not None:
+                print(slab.get('audio')[0], slab.get('volume'))
                 i += 1
             else:
                 sleep(0.1)
 
             if i == 10:
                 break
+
+        stop = time() * 1e6
+        print(f'{len(slab_reader.range(start, stop))}')
