cd1058a1901e6afad9d5efe1124fb029d7686a9b
GitHub CI Runner <thorwhalen1@gmail.com>
2022-11-18 16:38:37 +0000

    **CI** Formatted code + Updated version number and documentation. [skip ci]

--- plunk/sb/front_demo/user_story1/components/components.py
+++ plunk/sb/front_demo/user_story1/components/components.py
@@ -19,16 +19,16 @@ class AudioArrayDisplay(OutputBase):
 
         # arr = sf.read(BytesIO(sound), dtype="int16")[0]
         # st.write(type(arr))
-        tab1, tab2 = st.tabs(["Audio Player", "Waveform"])
+        tab1, tab2 = st.tabs(['Audio Player', 'Waveform'])
         with tab1:
             if not isinstance(sound, bytes):
                 sound = sound.getvalue()
-            arr = sf.read(BytesIO(sound), dtype="int16")[0]
+            arr = sf.read(BytesIO(sound), dtype='int16')[0]
 
             st.audio(arr)
         with tab2:
             fig, ax = plt.subplots(figsize=(15, 5))
-            ax.plot(sound, label=f"Tag={tag}")
+            ax.plot(sound, label=f'Tag={tag}')
             ax.legend()
             st.pyplot(fig)
             # st.write(arr[:10])
