4b254f282fbfaeb3ce0827b9a67bffba80a6fafe
Andie.Phan <Andie.Phan@analog.com>
2022-11-22 17:13:51 -0800

    audio storage: saving to file Fix encoding to wav bytes One test for both dict and file store.

--- plunk/ap/live_graph/audio_store.py
+++ plunk/ap/live_graph/audio_store.py
@@ -1,11 +1,12 @@
-import shutil
 from abc import ABC, abstractmethod
 from functools import cached_property
+from io import BytesIO
 from itertools import chain
 from math import ceil
 from operator import itemgetter
-from pathlib import Path
-from typing import MutableSequence, Callable
+from tempfile import TemporaryDirectory
+from typing import MutableSequence, Callable, MutableMapping
+from wave import Wave_write
 
 from dol import appendable, Files, wrap_kvs
 from mongodol.tracking_methods import (
@@ -15,8 +16,6 @@ from mongodol.tracking_methods import (
 )
 from recode import (
     encode_pcm_bytes,
-    decode_pcm_bytes,
-    encode_wav_header_bytes,
     decode_wav_bytes,
 )
 
@@ -84,8 +83,6 @@ N_TO_BULK = 10
 
 
 def should_bulk_if_bt_within_n(self, item, n=N_TO_BULK):
-    print(self, item, n)
-    print(self._bulk[0])
     bulk_ts, _ = audio_slab_kv(self._bulk[0])
     item_ts, _ = audio_slab_kv(item)
     return item_ts - bulk_ts < n
@@ -97,7 +94,6 @@ def join_ts_wf(self, bulk):
     :param bulk:
     :return:
     """
-    print(self, bulk)
     ts = bulk[0]['timestamp']
     joined_wf = list(chain.from_iterable(map(itemgetter('wf'), bulk)))
     return {'timestamp': ts, 'wf': joined_wf}
@@ -120,136 +116,103 @@ def bulk_append(
     return BulkAppend
 
 
+def ts_to_filename(ts, ext='.wav'):
+    return f'{ts}{ext}'
+
+
+def filename_to_ts(name, ext='.wav'):
+    return int(name[: -len(ext)])
+
+
+def wf_to_wav(
+    wf, sr: int = 44100, width_bytes: int = 2, *, n_channels: int = 1
+) -> bytes:
+    bio = BytesIO()
+    with Wave_write(bio) as obj:
+        obj.setparams((n_channels, width_bytes, sr, 0, 'NONE', 'not compressed'))
+        wf_bytes = encode_pcm_bytes(wf, width=width_bytes * 8, n_channels=n_channels)
+        obj.writeframes(wf_bytes)
+        bio.seek(0)
+    return bio.read()
+
+
+def wav_to_wf(wav):
+    wf, sr = decode_wav_bytes(wav)
+    return wf
+
+
+@track_method_calls(
+    tracked_methods='append',
+    tracking_mixin=bulk_append(),
+    calls_tracker=track_calls_without_executing,
+)
+@appendable(item2kv=audio_slab_kv)
+@wrap_kvs(
+    key_of_id=filename_to_ts,
+    id_of_key=ts_to_filename,
+    obj_of_data=wav_to_wf,
+    data_of_obj=wf_to_wav,
+)
+class WavFileStore(Files):
+    pass
+
+
+@track_method_calls(
+    tracked_methods='append',
+    tracking_mixin=bulk_append(),
+    calls_tracker=track_calls_without_executing,
+)
+@appendable(item2kv=audio_slab_kv)
+class DictStore(dict):
+    pass
+
+
 def _test_dict_store():
-    @track_method_calls(
-        tracked_methods='append',
-        tracking_mixin=bulk_append(),
-        calls_tracker=track_calls_without_executing,
-    )
-    @appendable(item2kv=audio_slab_kv)
-    class AudioStore(dict):
-        pass
-
-    n = 22
-    repeat = 2
-    # with AudioStore() as a:
-    #     for i in range(n):
-    #         a.append({'timestamp': 1, 'wf': 1})
-    #         print(i)
-    #         assert False, 'testing assert'
-    # print('print#0', '"assert False" did not stop run')
-    #
-    # with AudioStore() as a:
-    #     for i in range(n):
-    #         a.append({'timestamp': 1, 'wf': 1})
-    #         print(i)
-    #         raise Exception('Test Exception')
-    # print('print#1', '"Test Exception" did not stop run')
-
-    with AudioStore() as a:
-        for i in range(n):
-            a.append({'timestamp': i, 'wf': [i] * repeat})
-            print('print#2', f'{i=}', f'{len(a._tracks)=}', a)
-            assert len(a._tracks) == i + 1
-            print('print#3', f'{len(a) == 0=}')
-            assert len(a) == 0
-        print('print#4', f'{len(a._tracks) == n=}')
-        assert len(a._tracks) == n
-        print('print#5', a, len(a._tracks))
-
-    print('print#6', len(a._tracks), a)
-    assert len(a._tracks) == 0
-    assert len(a) == ceil(n / N_TO_BULK), f'{len(a)=} == {ceil(n / N_TO_BULK)=}'
-    for i, (k, v) in enumerate(sorted(a.items())):
-        assert k == i * N_TO_BULK
-        if k + N_TO_BULK > n:
-            assert len(v) / repeat == n - k, f'{i=}, {k=}, {len(v) / repeat=}, {n - k=}'
-        else:
-            assert (
-                len(v) / repeat == N_TO_BULK
-            ), f'{i=}, {k=}, {len(v) / repeat=}, {N_TO_BULK=}'
-        v_iter = iter(v)
-        for j in range(i * N_TO_BULK, min((i + 1) * N_TO_BULK, n)):
-            for _ in range(repeat):
-                assert (
-                    next(v_iter) == j
-                ), f'{i=}, {j=}, {k=}, {i * N_TO_BULK=}, {min((i + 1) * N_TO_BULK, n)=}'
+    _test_store(DictStore())
 
 
 def _test_files_store():
-    # WARNING: rootdir will be deleted at the start of test
-    rootdir = Path('/tmp/plunk_audio_store_test/')
-    if rootdir.is_dir():
-        shutil.rmtree(rootdir)
-    rootdir.mkdir()
+    with TemporaryDirectory() as tmpdirname:
+        _test_store(WavFileStore(rootdir=tmpdirname))
 
-    def ts_to_filename(ts, ext='.wav'):
-        return f'{ts}{ext}'
 
-    def filename_to_ts(name, ext='.wav'):
-        return int(name[: -len(ext)])
+def _test_store(store_instance: MutableMapping, *, n=22, chk_size=2, log=print):
+    """Test append, tracking, input, and output
 
-    def wf_to_wav(wf, *, sr=44100, width_bytes=2, n_channels=1):
-        header = encode_wav_header_bytes(
-            sr, width_bytes, n_channels=n_channels, nframes=len(wf)
-        )
-        pcm = encode_pcm_bytes(wf, width_bytes * 8, n_channels)
-        return header + pcm
-
-    def wav_to_wf(wav):
-        print(f'{wav=}')
-        wf, sr = decode_wav_bytes(wav)
-        return wf
-
-    @track_method_calls(
-        tracked_methods='append',
-        tracking_mixin=bulk_append(bulk_kv=join_ts_wf),
-        calls_tracker=track_calls_without_executing,
-    )
-    @appendable(item2kv=audio_slab_kv)
-    @wrap_kvs(
-        key_of_id=filename_to_ts,
-        id_of_key=ts_to_filename,
-        obj_of_data=wav_to_wf,
-        data_of_obj=wf_to_wav,
-    )
-    class AudioStore(Files):
-        def _id_of_key(self, k):
-            return super()._id_of_key(str(k))
-
-        # def _key_of_id(self, _id):
-        #     return int(super()._key_of_id(_id))
-
-    n = 22
-    repeat = 2
-
-    with AudioStore(rootdir='/tmp/plunk_audio_store_test/') as a:
+    :param store_instance:
+    :param n: number of chunks
+    :param chk_size: samples per chunk
+    :param log: print
+    :return:
+    """
+    with store_instance as a:
         for i in range(n):
-            a.append({'timestamp': i, 'wf': [i] * repeat})
-            print('print#2', f'{i=}', f'{len(a._tracks)=}', a)
+            a.append({'timestamp': i, 'wf': [i] * chk_size})
+            log('print#2', f'{i=}', f'{len(a._tracks)=}', a)
             assert len(a._tracks) == i + 1
-            print('print#3', f'{len(a) == 0=}')
+            log('print#3', f'{len(a) == 0=}')
             assert len(a) == 0, list(a)
-        print('print#4', f'{len(a._tracks) == n=}')
+        log('print#4', f'{len(a._tracks) == n=}')
         assert len(a._tracks) == n
-        print('print#5', a, len(a._tracks))
+        log('print#5', a, len(a._tracks))
 
-    print('print#6', len(a._tracks), a)
+    log('print#6', len(a._tracks), a)
     assert len(a._tracks) == 0
     assert len(a) == ceil(n / N_TO_BULK), f'{len(a)=} == {ceil(n / N_TO_BULK)=}'
-    for i, (_k, _v) in enumerate(sorted(a.items())):
-        k = int(_k)
-        v = decode_pcm_bytes(_v)
+    for i, (k, v) in enumerate(sorted(a.items())):
+        log('print#7', f'{(k, v)=}')
         assert k == i * N_TO_BULK
         if k + N_TO_BULK > n:
-            assert len(v) / repeat == n - k, f'{i=}, {k=}, {len(v) / repeat=}, {n - k=}'
+            assert (
+                len(v) / chk_size == n - k
+            ), f'{i=}, {k=}, {len(v) / chk_size=}, {n - k=}'
         else:
             assert (
-                len(v) / repeat == N_TO_BULK
-            ), f'{i=}, {k=}, {len(v) / repeat=}, {N_TO_BULK=}'
+                len(v) / chk_size == N_TO_BULK
+            ), f'{i=}, {k=}, {len(v) / chk_size=}, {N_TO_BULK=}'
         v_iter = iter(v)
         for j in range(i * N_TO_BULK, min((i + 1) * N_TO_BULK, n)):
-            for _ in range(repeat):
+            for _ in range(chk_size):
                 assert (
                     next(v_iter) == j
                 ), f'{i=}, {j=}, {k=}, {i * N_TO_BULK=}, {min((i + 1) * N_TO_BULK, n)=}'
