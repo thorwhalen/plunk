6c471133f12be09b10c5431924e34d724d9ef4b0
sylvainbonnot <sppbonnot@gmail.com>
2022-11-30 11:28:00 +0100

    use general crudifier

--- plunk/sb/front_demo/user_story1/utils/tools.py
+++ plunk/sb/front_demo/user_story1/utils/tools.py
@@ -179,3 +179,30 @@ def lined_dag(funcs):
         pairs = zip(names, names[1:])
         dag = dag.add_edges(pairs)
         return dag
+
+
+def assert_dims(wfs):
+    if wfs.ndim >= 2:
+        wfs = wfs[:, 0]
+    return wfs
+
+
+def tagged_sound_to_array(train_audio: WaveForm, tag: str):
+    sound, tag = train_audio, tag
+    if not isinstance(sound, bytes):
+        sound = sound.getvalue()
+
+    arr = sf.read(BytesIO(sound), dtype="int16")[0]
+    return arr, tag
+
+
+def tagged_sounds_to_single_array(train_audio: List[WaveForm], tag: str):
+    sounds, tag = train_audio, tag
+    result = []
+    for sound in sounds:
+        # if not isinstance(sound, bytes):
+        sound = sound.getvalue()
+        arr = sf.read(BytesIO(sound), dtype="int16")[0]
+        result.append(arr)
+    # print(np.hstack(result))
+    return np.hstack(result).reshape(-1, 1), tag
