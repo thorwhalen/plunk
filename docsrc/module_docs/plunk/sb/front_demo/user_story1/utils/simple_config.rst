0f3fdb4638973d4f3c5989c33e3364523a59970c
GitHub CI Runner <thorwhalen1@gmail.com>
2022-12-01 16:47:33 +0000

    **CI** Formatted code + Updated version number and documentation. [skip ci]

---
+++ docsrc/module_docs/plunk/sb/front_demo/user_story1/utils/simple_config.rst
@@ -0,0 +1,4 @@
+plunk.sb.front_demo.user_story1.utils.simple_config
+===================================================
+.. automodule:: plunk.sb.front_demo.user_story1.utils.simple_config
+   :members:
--- plunk/sb/front_demo/user_story1/utils/simple_config.py
+++ plunk/sb/front_demo/user_story1/utils/simple_config.py
@@ -1,13 +1,9 @@
 from dataclasses import dataclass
 from typing import Callable, Dict
 from front import ELEMENT_KEY
-from streamlitfront.elements import (
-    SuccessNotification,
-)
+from streamlitfront.elements import SuccessNotification
 from i2 import Sig
-from streamlitfront.elements import (
-    FileUploader,
-)
+from streamlitfront.elements import FileUploader
 
 
 def merge_dicts(d1, d2):
@@ -16,18 +12,13 @@ def merge_dicts(d1, d2):
 
 def overwrite_dict(d1, d2):
     for key, val in d2.items():
-        print(f"{key=}, {val=}")
+        print(f'{key=}, {val=}')
         d1[key] = val
     return d1
 
 
 dflt_template = {
-    "execution": {
-        "inputs": dict(),
-        "output": {
-            ELEMENT_KEY: SuccessNotification,
-        },
-    },
+    'execution': {'inputs': dict(), 'output': {ELEMENT_KEY: SuccessNotification,},},
 }
 
 
@@ -43,9 +34,9 @@ class Component:  # more like preferences
         s = dflt_template
         sig = Sig(self.func)
 
-        s["execution"]["inputs"] = {arg_name: dict() for arg_name in sig.names}
+        s['execution']['inputs'] = {arg_name: dict() for arg_name in sig.names}
 
-        s = KeyPath(".")(s)
+        s = KeyPath('.')(s)
 
         return s
 
@@ -54,23 +45,23 @@ class Component:  # more like preferences
     ):  # use may be the keypath here only for the overwrites
         # might need to cast the overwrites and also the output to keypath
         # overwrites  = KeyPath('.')(overwrites)
-        result = KeyPath(".")(overwrite_dict(self.configs, overwrites))
+        result = KeyPath('.')(overwrite_dict(self.configs, overwrites))
         return result
 
     __call__ = mk_configs
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from plunk.sb.front_demo.user_story1.utils.funcs import upload_sound
 
     upload_component = Component(func=upload_sound)
     print(upload_component.configs)
     result = upload_component.mk_configs(
         {
-            "execution.inputs.train_audio": {
+            'execution.inputs.train_audio': {
                 ELEMENT_KEY: FileUploader,
-                "type": "wav",
-                "accept_multiple_files": True,
+                'type': 'wav',
+                'accept_multiple_files': True,
             },
         }
     )
