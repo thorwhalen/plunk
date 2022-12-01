50e0e36071100ef39670546e1da448200be9eb09
Andie.Phan <Andie.Phan@analog.com>
2022-11-30 17:15:36 -0800

    live_graph_data_buffer.py fixed bug tracking now flushes after SlabsIter exits

--- plunk/ap/live_graph/audio_store.py
+++ plunk/ap/live_graph/audio_store.py
@@ -49,6 +49,12 @@ class BulkStore(Protocol):
     def __iter__(self) -> Iterator:
         pass
 
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
 
 audio_slab_kv = itemgetter('timestamp', 'wf')
 
