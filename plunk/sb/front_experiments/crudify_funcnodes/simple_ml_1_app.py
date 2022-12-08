e30571d08618b6c8c2ade8eedce8d7b17579d2c5
sylvainbonnot <sppbonnot@gmail.com>
2022-12-08 15:20:50 +0100

    update

--- plunk/sb/front_experiments/crudify_funcnodes/simple_ml_1_app.py
+++ plunk/sb/front_experiments/crudify_funcnodes/simple_ml_1_app.py
@@ -3,49 +3,70 @@ from know.boxes import *
 from streamlitfront.elements import FileUploader
 from front import APP_KEY, RENDERING_KEY, ELEMENT_KEY, NAME_KEY
 from streamlitfront import mk_app, binder as b
+from typing import Mapping
 
 
 from front.dag import crudify_funcs
-from front.crude import DillFiles
-import os
-from collections import defaultdict
 
-mall = dict()
-# mall = DillFiles(os.path.expanduser('~/tmp'))
-# mall = defaultdict()
 
-audio_anomalies = sml.audio_anomalies
+# filepath = /Users/sylvain/Dropbox/Otosense/VacuumEdgeImpulse/train/noise.AirConditioner_2.9.1440000-1600000.wav.23q8e34o.ingestion-6bc8b65f8c-vrv59.wav
 
-it = crudify_funcs(var_nodes="wf model results", dag=audio_anomalies, mall=mall)
-step1, step2, step3 = list(it)
 
-from i2 import Sig
+def mk_pipeline_maker_app_with_mall(
+    mall: Mapping,
+    *,
+    pipelines: str = None,
+    sound_output: str = None,
+    models_scores: str = None
+):
 
-print(Sig(step1))
+    if not b.mall():
+        b.mall = mall
+    mall = b.mall()
+    store = dict()
+    mall["wf_store"] = store
+    audio_anomalies = sml.audio_anomalies
 
-config = {
-    APP_KEY: {"title": "Data Preparation"},
-    RENDERING_KEY: {
-        "step1": {
-            "execution": {
-                "inputs": {
-                    "audio_source": {
-                        ELEMENT_KEY: FileUploader,
-                        "type": "wav",
-                        "accept_multiple_files": True,
+    it = crudify_funcs(var_nodes="wf model results", dag=audio_anomalies, mall=mall)
+    # it = crudify_funcs(var_nodes="wf", dag=audio_anomalies, mall=mall)
+    print(audio_anomalies.synopsis_string())
+
+    step1, step2, step3 = list(it)
+
+    config = {
+        APP_KEY: {"title": "Data Preparation"},
+        RENDERING_KEY: {
+            "step1": {
+                "execution": {
+                    "inputs": {
+                        "audio_source": {
+                            ELEMENT_KEY: FileUploader,
+                            "type": "wav",
+                            "accept_multiple_files": True,
+                        },
                     },
                 },
             },
+            # "learn_apply_model": {
+            #     NAME_KEY: "Apply model",
+            #     #'execution': {'output': {ELEMENT_KEY: ArrayPlotter,},},
+            # },
         },
-        # "learn_apply_model": {
-        #     NAME_KEY: "Apply model",
-        #     #'execution': {'output': {ELEMENT_KEY: ArrayPlotter,},},
-        # },
-    },
-}
+    }
+    funcs = [step1]
+    app = mk_app(funcs, config=config)
 
-funcs = [step1]
-app = mk_app(funcs, config=config)
-app()
+    return app
 
-# filepath = /Users/sylvain/Dropbox/Otosense/VacuumEdgeImpulse/train/noise.AirConditioner_2.9.1440000-1600000.wav.23q8e34o.ingestion-6bc8b65f8c-vrv59.wav
+
+if __name__ == "__main__":
+
+    mall = dict(
+        # sound_output=dict(),
+        # pipelines=dict(),
+        # models_scores=dict(),
+    )
+
+    app = mk_pipeline_maker_app_with_mall(mall)
+
+    app()
