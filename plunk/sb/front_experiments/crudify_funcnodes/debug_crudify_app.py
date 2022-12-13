c282ba1aa03e334b4344c485079dc8438c2bd2ce
GitHub CI Runner <thorwhalen1@gmail.com>
2022-12-13 16:45:37 +0000

    **CI** Formatted code + Updated version number and documentation. [skip ci]

--- plunk/sb/front_experiments/crudify_funcnodes/debug_crudify_app.py
+++ plunk/sb/front_experiments/crudify_funcnodes/debug_crudify_app.py
@@ -9,12 +9,12 @@ from meshed import code_to_dag
 from streamlitfront.elements import SelectBox
 
 
-def foo(x, y, z="23"):
+def foo(x, y, z='23'):
     return int(x) + int(y) * int(z)
 
 
-def bar(res, other="stuff"):
-    return f"{res=}, {other=}"
+def bar(res, other='stuff'):
+    return f'{res=}, {other=}'
 
 
 from i2 import include_exclude, rm_params
@@ -26,21 +26,16 @@ def dag():
     result = second_func(res, other)
 
 
-dag = dag.ch_funcs(
-    first_func=rm_params(foo, params_to_remove=("z",)),
-    second_func=bar,
-)
+dag = dag.ch_funcs(first_func=rm_params(foo, params_to_remove=('z',)), second_func=bar,)
 
 
-def mk_pipeline_maker_app_with_mall(
-    mall: Mapping,
-):
+def mk_pipeline_maker_app_with_mall(mall: Mapping,):
 
     if not b.mall():
         b.mall = mall
     mall = b.mall()
 
-    _funcs = crudify_funcs(var_nodes="res result", dag=dag, mall=mall)
+    _funcs = crudify_funcs(var_nodes='res result', dag=dag, mall=mall)
 
     print(dag.synopsis_string())
 
@@ -50,19 +45,16 @@ def mk_pipeline_maker_app_with_mall(
 
     step1, step2 = _funcs
 
-    print(f"{step1.__name__ =}")
-    print(f"{step2.__name__ =}")
+    print(f'{step1.__name__ =}')
+    print(f'{step2.__name__ =}')
 
     config = {
-        APP_KEY: {"title": "Data Preparation"},
+        APP_KEY: {'title': 'Data Preparation'},
         RENDERING_KEY: {
-            "step2": {
-                "execution": {
-                    "inputs": {
-                        "bar": {
-                            ELEMENT_KEY: SelectBox,
-                            "options": mall["res_store"],
-                        },
+            'step2': {
+                'execution': {
+                    'inputs': {
+                        'bar': {ELEMENT_KEY: SelectBox, 'options': mall['res_store'],},
                     },
                 },
             },
@@ -79,12 +71,10 @@ def mk_pipeline_maker_app_with_mall(
     return app
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import streamlit as st
 
-    mall = dict(
-        res_store=dict(),
-    )
+    mall = dict(res_store=dict(),)
 
     app = mk_pipeline_maker_app_with_mall(mall)
 
