70d85638e74877c96b2c7b42460eb4b6071414cf
GitHub CI Runner <thorwhalen1@gmail.com>
2022-12-15 13:53:30 +0000

    **CI** Formatted code + Updated version number and documentation. [skip ci]

--- plunk/sb/front_experiments/store_explorer/andie_app2.py
+++ plunk/sb/front_experiments/store_explorer/andie_app2.py
@@ -21,22 +21,22 @@ from plunk.ap.store_explorer.store_explorer_element import StoreExplorer
 class StoreExplorer(OutputBase):
     def render(self):
         store = self.output
-        print(f"{store=}")
+        print(f'{store=}')
         # Store the initial value of widgets in session state
         if not b.depth_keys():
             b.depth_keys = []
             b.render_count = 0
 
-        print(f"Start {b.render_count()=} {b.depth_keys()=}")
+        print(f'Start {b.render_count()=} {b.depth_keys()=}')
 
         col1, col2 = st.columns(2)
         with col1:
-            st.write("# Keys")
+            st.write('# Keys')
             for i in range(len(b.depth_keys()) + 1):
                 if i == 0:
                     d = store
                 elif len(b.depth_keys()) == i and (b.depth_keys()[i - 1]) is not None:
-                    st.write(f"walrus activated")
+                    st.write(f'walrus activated')
                     dk = b.depth_keys()[i - 1]
                     d = d[dk]
                 else:
@@ -44,37 +44,35 @@ class StoreExplorer(OutputBase):
 
                 if isinstance(d, dict):
                     options = [None, *d]
-                    k = st.selectbox(key := f"depth_{i}", options=options, key=key)
+                    k = st.selectbox(key := f'depth_{i}', options=options, key=key)
                     b.depth_keys = [*b.depth_keys()[:i], k]
-                    st.write(f"current {b.depth_keys()=}")
+                    st.write(f'current {b.depth_keys()=}')
                 if isinstance(d, list):
                     options = [None, *(j for j in range(len(d)))]
-                    k = st.selectbox(key := f"depth_{i}", options=options, key=key)
+                    k = st.selectbox(key := f'depth_{i}', options=options, key=key)
                     b.depth_keys = [*b.depth_keys()[:i], k]
             if k is not None:
-                print(f"{k=}")
-                st.write("Value")
+                print(f'{k=}')
+                st.write('Value')
                 st.write(d)
 
         with col2:
-            st.write("# Store")
+            st.write('# Store')
             st.write(store)
 
-        print(f"{b.depth_keys()=}")
-        print(f"Finish {b.render_count()=}")
+        print(f'{b.depth_keys()=}')
+        print(f'Finish {b.render_count()=}')
         b.render_count = b.render_count() + 1
 
 
-def mk_pipeline_maker_app_with_mall(
-    mall: Mapping,
-):
+def mk_pipeline_maker_app_with_mall(mall: Mapping,):
     if not b.mall():
         b.mall = mall
     mall = b.mall()
 
     crudifier = partial(Crudifier, mall=mall)
 
-    @crudifier(output_store="sound_output")
+    @crudifier(output_store='sound_output')
     def upload_sound(train_audio: list, tag: str):
         return train_audio, tag
 
@@ -82,34 +80,34 @@ def mk_pipeline_maker_app_with_mall(
         return mall[key]
 
     config = {
-        APP_KEY: {"title": "Data Preparation"},
+        APP_KEY: {'title': 'Data Preparation'},
         RENDERING_KEY: {
-            "upload_sound": {
+            'upload_sound': {
                 # NAME_KEY: "Data Loader",
-                "execution": {
-                    "inputs": {
-                        "train_audio": {
+                'execution': {
+                    'inputs': {
+                        'train_audio': {
                             ELEMENT_KEY: FileUploader,
-                            "type": "wav",
-                            "accept_multiple_files": True,
+                            'type': 'wav',
+                            'accept_multiple_files': True,
                         },
                     },
-                    "output": {
+                    'output': {
                         ELEMENT_KEY: SuccessNotification,
-                        "message": "Wav files loaded successfully.",
+                        'message': 'Wav files loaded successfully.',
                     },
                 },
             },
-            "explore_mall": {
-                "execution": {
-                    "inputs": {
-                        "key": {
+            'explore_mall': {
+                'execution': {
+                    'inputs': {
+                        'key': {
                             ELEMENT_KEY: SelectBox,
-                            "options": list(mall),
-                            "value": b.selected_key,
+                            'options': list(mall),
+                            'value': b.selected_key,
                         }
                     },
-                    "output": {ELEMENT_KEY: StoreExplorer},
+                    'output': {ELEMENT_KEY: StoreExplorer},
                 }
             },
         },
@@ -130,15 +128,15 @@ mall = dict(
     # Output Store
     data=dict(
         a=1,
-        b="2",
+        b='2',
         c=[3],
-        d={"4": 4, "5": [{"e": 6}, {"f": 7}, ["g", "h", "i"]]},
-        j=[{"k": 8, "l": 9}, "m", 10],
+        d={'4': 4, '5': [{'e': 6}, {'f': 7}, ['g', 'h', 'i']]},
+        j=[{'k': 8, 'l': 9}, 'm', 10],
     ),
 )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     app = mk_pipeline_maker_app_with_mall(mall)
 
