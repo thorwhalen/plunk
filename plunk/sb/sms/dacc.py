e881db991844173a1627b9ada27b823e4e289def
sylvainbonnot <sppbonnot@gmail.com>
2023-02-01 17:51:39 +0100

    update

---
+++ plunk/sb/sms/dacc.py
@@ -0,0 +1,26 @@
+from tabled import DfLocalFileReader as Reader
+from plunk.sb.sms.utils import metadata_store, data_store
+from dol import lazyprop
+
+import os
+
+
+path = "~/Dropbox/_odata/sound/induction_motor_data/Healthy/Healthy_750rpm/"
+DFLT_LOCAL_SOURCE_DIR = os.path.expanduser(path)
+
+
+def mk_dacc():
+    return Dacc(root_dir=DFLT_LOCAL_SOURCE_DIR)
+
+
+class Dacc:
+    def __init__(self, root_dir=DFLT_LOCAL_SOURCE_DIR):
+        self.global_store = Reader(root_dir)
+
+    @lazyprop
+    def metadata(self):
+        return metadata_store(self.global_store)
+
+    @lazyprop
+    def data(self):
+        return data_store(self.global_store)
--- plunk/sb/sms/utils.py
+++ plunk/sb/sms/utils.py
@@ -11,10 +11,12 @@ import os
 import pickle
 
 import pandas as pd
-from functools import partial
-from py2store.stores.local_store import LocalBinaryStore
 from dol import wrap_kvs
 from lined import Pipe
+from tabled import (
+    DfLocalFileReader,
+    dflt_ext_mapping,
+)
 
 
 DFLT_EXT_SPECS = {}
@@ -36,67 +38,8 @@ def df_from_json(data, ext_specs={"orient": "index"}, **kwargs):
     return pd.read_json(data, **kwargs).T  # modified from tabled
 
 
-def df_from_data_given_ext(data, ext, ext_specs=None, **kwargs):
-    """Get a dataframe from a (data, ext) pair"""
-    ext_specs = ext_specs or DFLT_EXT_SPECS  # NOTE: Note used yet
-    if ext.startswith("."):
-        ext = ext[1:]
-    if ext in {"xls", "xlsx"}:
-        kwargs = dict({"index": False}, **kwargs)
-        return pd.read_excel(data, **kwargs)
-    elif ext in {"csv"}:
-        kwargs = dict({"index_col": False}, **kwargs)
-        return pd.read_csv(data, **kwargs)
-    elif ext in {"tsv"}:
-        kwargs = dict({"sep": "\t", "index_col": False}, **kwargs)
-        return pd.read_csv(data, **kwargs)
-    elif ext in {"json"}:
-        kwargs = dict({"orient": "index"}, **kwargs)
-        # kwargs = dict({"orient": "split"}, **kwargs)
+dflt_ext_mapping["json"] = df_from_json
 
-        return pd.read_json(data, **kwargs).T
-    elif ext in {"html"}:
-        kwargs = dict({"index_col": False}, **kwargs)
-        return pd.read_html(data, **kwargs)[0]
-    elif ext in {"p", "pickle"}:
-        return pickle.load(data, **kwargs)
-    else:
-        raise ValueError(f"Don't know how to handle extension: {ext}")
-
-
-class DfLocalFileReader(LocalBinaryStore):
-    """A key-value store providing values as pandas.DataFrames"""
-
-    def __init__(self, path_format, ext_specs=None):
-        super().__init__(path_format)
-        if ext_specs is None:
-            ext_specs = DFLT_EXT_SPECS
-        self._ext_specs = ext_specs
-        self.data_and_ext_to_df = partial(df_from_data_given_ext, ext_specs=ext_specs)
-        self.data_and_ext_to_df = (
-            df_from_data_given_ext  # TODO: Hard coded for now, to keep functioning
-        )
-
-    def __getitem__(self, k):
-        ext = self.key_to_ext(k)
-        kwargs = self._ext_specs.get(ext, {})
-        data = BytesIO(super().__getitem__(k))
-        return df_from_data_given_ext(data, ext, **kwargs)
-
-    def key_to_ext(self, k):
-        _, ext = os.path.splitext(k)
-        if ext.startswith("."):
-            ext = ext[1:]
-        return ext
-
-    def __setitem__(self, k, v):
-        raise NotImplementedError("This is a reader: No write operation allowed")
-
-    def __delitem__(self, k):
-        raise NotImplementedError("This is a reader: No delete operation allowed")
-
-
-DfReader = DfLocalFileReader  # alias for back-compatibility: TODO: Issue warning on use
 DFLT_METADATA_COLS = [
     "dataType",
     "deviceId",
@@ -141,9 +84,11 @@ if __name__ == "__main__":
     path = "~/Dropbox/_odata/sound/induction_motor_data/"
     full_path = os.path.expanduser(path)
 
+    dflt_ext_mapping["json"] = df_from_json
+
     # store
     store = DfLocalFileReader(full_path)
 
     # try one key
     key = list(store.keys())[0]
-    store[key]
+    print(store[key])
