db982025a74165f8095c001ca7baec66ca032be7
sylvainbonnot <sppbonnot@gmail.com>
2023-03-06 15:31:33 +0100

    update

--- plunk/sb/front_demo/user_story1/apps/app5_drill.py
+++ plunk/sb/front_demo/user_story1/apps/app5_drill.py
@@ -3,11 +3,9 @@ An app that loads either a wav file from local folder or records a sound
 and visualizes the resulting numpy array 
 """
 from functools import partial
-from typing import Callable, Iterable, List, Mapping
+from typing import List, Mapping
 from i2 import FuncFactory, Sig
 from lined import LineParametrized
-import numpy as np
-
 
 from front import APP_KEY, RENDERING_KEY, ELEMENT_KEY, NAME_KEY
 from front.crude import Crudifier
@@ -31,28 +29,12 @@ from olab.types import (
 )
 from olab.util import (
     simple_chunker,
-    apply_func_to_index_groups,
-    arg_top_max,
-    clean_dict,
-    indices_for_percentile,
-    consecutive_indices,
-    get_groups_extremities_all,
     scores_to_intervals,
     simple_featurizer,
-    tagged_sounds_to_single_array,
-    assert_dims,
     ArrayWithIntervalsPlotter,
 )
 
-from olab.base import mk_step
-
-
-from omodel.outliers.pystroll import OutlierModel as Stroll
-
-
-DFLT_CHK_SIZE = 2048
-DFLT_FEATURIZER = lambda chk: np.abs(np.fft.rfft(chk))
-DFLT_NUM_OUTLIERS = 3
+from olab.base import mk_step, mk_pipeline, learn_outlier_model, apply_fitted_model
 
 
 def mk_pipeline_maker_app_with_mall(
@@ -81,51 +63,30 @@ def mk_pipeline_maker_app_with_mall(
     data_store = data_store or data
     pipelines_store = pipelines_store or pipelines
 
-    mk_step = crudifier(
+    mk_step_crudified = crudifier(
         param_to_mall_map=dict(step_factory=step_factories), output_store=steps_store
     )(mk_step)
 
     #
-    @crudifier(
+    mk_pipeline_crudified = crudifier(
         output_store=pipelines_store,
-    )
-    def mk_pipeline(steps: Iterable[Callable]):
-        return LineParametrized(*steps)
+    )(mk_pipeline)
 
-    @crudifier(
+    learn_outlier_model_crudified = crudifier(
         param_to_mall_map=dict(
             tagged_data="sound_output", preprocess_pipeline="pipelines"
         ),
         output_store="learned_models",
-    )
-    def learn_outlier_model(tagged_data, preprocess_pipeline, n_centroids=5):
-        sound, _ = tagged_sounds_to_single_array(*tagged_data)
-        wfs = np.array(sound)
+    )(learn_outlier_model)
 
-        wfs = assert_dims(wfs)
-
-        fvs = preprocess_pipeline(wfs)
-        model = Stroll(n_centroids=n_centroids)
-        model.fit(X=fvs)
-
-        return model
-
-    @crudifier(
+    apply_fitted_model_crudified = crudifier(
         param_to_mall_map=dict(
             tagged_data="sound_output",
             preprocess_pipeline="pipelines",
             fitted_model="learned_models",
         ),
         output_store="models_scores",
-    )
-    def apply_fitted_model(tagged_data, preprocess_pipeline, fitted_model):
-        sound, _ = tagged_sounds_to_single_array(*tagged_data)
-        wfs = np.array(sound)
-        wfs = assert_dims(wfs)
-
-        fvs = preprocess_pipeline(wfs)
-        scores = fitted_model.score_samples(X=fvs)
-        return scores
+    )(apply_fitted_model)
 
     @crudifier(
         param_to_mall_map=dict(pipeline=pipelines_store),
@@ -145,17 +106,7 @@ def mk_pipeline_maker_app_with_mall(
 
     @crudifier(output_store="sound_output")
     def upload_sound(train_audio: List[WaveForm], tag: str):
-        # sound, tag = train_audio, tag
-        # if not isinstance(sound, bytes):
-        #     sound = sound.getvalue()
-
-        # arr = sf.read(BytesIO(sound), dtype='int16')[0]
-        # return arr, tag
         return train_audio, tag
-
-    @crudifier(param_to_mall_map={"result": "sound_output"})
-    def display_tag_sound(result):
-        return result
 
     def get_step_name(step):
         return [k for k, v in mall[steps].items() if v == step][0]
@@ -193,7 +144,7 @@ def mk_pipeline_maker_app_with_mall(
                     },
                 },
             },
-            "mk_step": {
+            "mk_step_crudified": {
                 NAME_KEY: "Pipeline Step Maker",
                 "execution": {
                     "inputs": {
@@ -208,7 +159,7 @@ def mk_pipeline_maker_app_with_mall(
                     },
                 },
             },
-            "mk_pipeline": {
+            "mk_pipeline_crudified": {
                 NAME_KEY: "Pipeline Maker",
                 "execution": {
                     "inputs": {
@@ -282,12 +233,10 @@ def mk_pipeline_maker_app_with_mall(
 
     funcs = [
         upload_sound,
-        # display_tag_sound,
-        mk_step,
-        mk_pipeline,
-        learn_outlier_model,
-        apply_fitted_model,
-        # exec_pipeline,
+        mk_step_crudified,
+        mk_pipeline_crudified,
+        learn_outlier_model_crudified,
+        apply_fitted_model_crudified,
         visualize_pipeline,
         visualize_scores,
     ]
@@ -312,16 +261,6 @@ mall = dict(
     learned_models=dict(),
     models_scores=dict(),
 )
-
-# crudifier = partial(prepare_for_crude_dispatch, mall=mall)
-
-# step_factories = dict(
-#     # ML
-#     chunker=FuncFactory(simple_chunker),
-#     featurizer=FuncFactory(simple_featurizer),
-# )
-
-# mall['step_factories'] = step_factories
 
 
 if __name__ == "__main__":
