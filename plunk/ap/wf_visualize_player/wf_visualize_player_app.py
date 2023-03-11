44d2973c89d85269554228fc1fa4efb8d21ce747
Andie.Phan <andie.phan@analog.com>
2023-03-10 17:26:36 -0800

    Waveform Visualization Player

--- plunk/ap/wf_visualize_player/wf_visualize_player_app.py
+++ plunk/ap/wf_visualize_player/wf_visualize_player_app.py
@@ -1,39 +1,107 @@
-from front import APP_KEY, RENDERING_KEY, NAME_KEY, ELEMENT_KEY
-from streamlitfront import mk_app
+from typing import Tuple
 
-from plunk.ap.wf_visualize_player.test_data import (
-    wf_sine,
-    wf_bleeps,
-    wf_mix,
-    wf_pure_tone,
+import numpy as np
+from hum import (
+    mk_sine_wf,
+    DFLT_N_SAMPLES,
+    DFLT_SR,
+    wf_with_timed_bleeps,
+    freq_based_stationary_wf,
+    pure_tone,
 )
-from plunk.ap.wf_visualize_player.wf_visualize_player_element import WfVisualizePlayer
+
+from plunk.ap.wf_visualize_player.wf_visualize_player_element import (
+    WfVisualizePlayer,
+    WfSr,
+)
+
+
+def wf_sine() -> WfSr:
+    return (
+        mk_sine_wf(freq=20, n_samples=DFLT_N_SAMPLES, sr=DFLT_SR, phase=0.25, gain=3),
+        DFLT_SR,
+    )
+
+
+def wf_bleeps() -> WfSr:
+    return (
+        wf_with_timed_bleeps(
+            n_samples=DFLT_N_SAMPLES, bleep_loc=1024 * 4, bleep_spec=1024, sr=DFLT_SR,
+        ),
+        DFLT_SR,
+    )
+
+
+def wf_mix() -> WfSr:
+    return (
+        freq_based_stationary_wf(
+            freqs=(2, 4, 6, 8), weights=None, n_samples=DFLT_N_SAMPLES, sr=DFLT_SR,
+        ),
+        DFLT_SR,
+    )
+
+
+def wf_pure_tone() -> WfSr:
+    return (
+        pure_tone(chk_size=DFLT_N_SAMPLES, freq=440, sr=DFLT_SR, max_amplitude=30000),
+        DFLT_SR,
+    )
+
+
+def wf_two_channel_sine_tone() -> WfSr:
+    s, _ = wf_sine()
+    t, sr = wf_pure_tone()
+    return np.array([s, t]), sr
+
 
 if __name__ == '__main__':
+    from front import APP_KEY, RENDERING_KEY, NAME_KEY, ELEMENT_KEY
+    from streamlitfront import mk_app
+
     app = mk_app(
-        [wf_sine, wf_bleeps, wf_mix, wf_pure_tone],
+        [wf_sine, wf_bleeps, wf_mix, wf_pure_tone, wf_two_channel_sine_tone],
         config={
             APP_KEY: {'title': 'Waveform Visualization Player'},
             RENDERING_KEY: {
                 'wf_sine': {
                     NAME_KEY: 'Sine',
                     'description': {'content': 'Sine Waveform'},
-                    'execution': {'output': {ELEMENT_KEY: WfVisualizePlayer},},
+                    'execution': {
+                        'output': {ELEMENT_KEY: WfVisualizePlayer},
+                        'auto_submit': True,
+                    },
                 },
                 'wf_bleeps': {
                     NAME_KEY: 'Bleeps',
                     'description': {'content': 'Bleeps Waveform'},
-                    'execution': {'output': {ELEMENT_KEY: WfVisualizePlayer},},
+                    'execution': {
+                        'output': {ELEMENT_KEY: WfVisualizePlayer},
+                        'auto_submit': True,
+                    },
                 },
                 'wf_mix': {
                     NAME_KEY: 'Mixed',
                     'description': {'content': 'Mixed Frequency Waveform'},
-                    'execution': {'output': {ELEMENT_KEY: WfVisualizePlayer},},
+                    'execution': {
+                        'output': {ELEMENT_KEY: WfVisualizePlayer},
+                        'auto_submit': True,
+                    },
                 },
                 'wf_pure_tone': {
                     NAME_KEY: 'Pure Tone',
                     'description': {'content': 'Pure Tone Waveform'},
-                    'execution': {'output': {ELEMENT_KEY: WfVisualizePlayer},},
+                    'execution': {
+                        'output': {ELEMENT_KEY: WfVisualizePlayer},
+                        'auto_submit': True,
+                    },
+                },
+                'wf_two_channel_sine_tone': {
+                    NAME_KEY: '2 Channel',
+                    'description': {'content': 'Sine & Pure Tone Waveform'},
+                    'execution': {
+                        'output': {ELEMENT_KEY: WfVisualizePlayer},
+                        'auto_submit': True,
+                    },
                 },
             },
         },
--- plunk/ap/wf_visualize_player/wf_visualize_player_element.py
+++ plunk/ap/wf_visualize_player/wf_visualize_player_element.py
@@ -9,6 +9,8 @@ from front.elements import OutputBase
 
 from plunk.ap.live_graph.live_graph_streamlitfront import plot, line_plot
 
+WfSr = Tuple[np.ndarray, int]
+
 
 def only_if_output(f):
     @wraps(f)
@@ -19,9 +21,22 @@ def only_if_output(f):
     return wrapper
 
 
-def spectrum_plot(graph_data: Iterable[Union[int, float]], title: str, figsize=(15, 5)):
+FIG_SIZE = (15, 2)
+
+
+def line_plot(graph_data: Iterable[Union[int, float]], title: str, figsize=FIG_SIZE):
     fig, ax = plt.subplots(figsize=figsize)
-    st.markdown(f'## {title}')
+    st.markdown(f'### {title}')
+    ax.plot(graph_data)
+    st.pyplot(fig)
+    plt.close(fig)
+
+
+def spectrum_plot(
+    graph_data: Iterable[Union[int, float]], title: str, figsize=FIG_SIZE
+):
+    fig, ax = plt.subplots(figsize=figsize)
+    st.markdown(f'### {title}')
     ax.specgram(graph_data)
     st.pyplot(fig)
     plt.close(fig)
@@ -31,20 +46,31 @@ def audio_player(wf, sr):
     st.audio(wf, sample_rate=sr)
 
 
+def render_channel(channel_index, wf, sr):
+    box = st.empty()
+    with box.container():
+        st.markdown(f'## channel-{channel_index}')
+        line_plot(wf, 'Waveform')
+        spectrum_plot(wf, 'Spectrum')
+        audio_player(wf, sr)
+
+
 @dataclass
 class WfVisualizePlayer(OutputBase):
-    output: Tuple[np.ndarray, int] = None
+    output: WfSr = None
 
     @property
-    def wf(self):
+    def wf(self) -> np.ndarray:
         return self.output[0]
 
     @property
-    def sr(self):
+    def sr(self) -> int:
         return self.output[1]
 
     @only_if_output
     def render(self):
-        line_plot(self.wf, 'Waveform')
-        spectrum_plot(self.wf, 'Spectrum')
-        audio_player(self.wf, self.sr)
+        if self.wf.ndim > 1:
+            for i, ch_wf in enumerate(self.wf):
+                render_channel(i, ch_wf, self.sr)
+        else:
+            render_channel(0, self.wf, self.sr)
