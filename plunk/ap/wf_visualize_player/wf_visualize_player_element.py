b146b7db777daa60b434599f273e3766b06a2ab5
Andie.Phan <andie.phan@analog.com>
2023-03-15 13:49:15 -0700

    timescale on data viz

--- plunk/ap/wf_visualize_player/wf_visualize_player_element.py
+++ plunk/ap/wf_visualize_player/wf_visualize_player_element.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass
 from functools import wraps
-from typing import Iterable, Union, Tuple
+from typing import Iterable, Union, Tuple, Sequence, Callable
 
 import streamlit as st
 import numpy as np
 from matplotlib import pyplot as plt
 from front.elements import OutputBase
+from matplotlib.axes import Axes
 
-from plunk.ap.live_graph.live_graph_streamlitfront import plot, line_plot
+from plunk.ap.live_graph.live_graph_streamlitfront import line_plot
 
 WfSr = Tuple[np.ndarray, int]
 
@@ -24,22 +25,30 @@ def only_if_output(f):
 FIG_SIZE = (15, 2)
 
 
-def line_plot(graph_data: Iterable[Union[int, float]], title: str, figsize=FIG_SIZE):
-    fig, ax = plt.subplots(figsize=figsize)
-    st.markdown(f'### {title}')
-    ax.plot(graph_data)
-    st.pyplot(fig)
-    plt.close(fig)
+def spectrum_plot(ax: Axes, graph_data: Sequence[Union[int, float]], sr=None):
+    ax.specgram(graph_data, Fs=sr)
 
 
-def spectrum_plot(
-    graph_data: Iterable[Union[int, float]], title: str, figsize=FIG_SIZE
+def line_plot(ax: Axes, graph_data: Sequence[Union[int, float]], sr=None):
+    time = np.arange(len(graph_data)) / sr
+    ax.plot(time, graph_data)
+
+
+def plot_data(
+    title: str,
+    graph_data: Sequence[Union[int, float]],
+    sr: int,
+    plot_ax: Callable[[Axes, Sequence[Union[int, float]], int], None],
+    figsize=FIG_SIZE,
 ):
     fig, ax = plt.subplots(figsize=figsize)
-    st.markdown(f'### {title}')
-    ax.specgram(graph_data)
+    st.markdown(f'##### {title}')
+    plot_ax(ax, graph_data, sr)
+    ax.margins(x=0)
     st.pyplot(fig)
     plt.close(fig)
+
+
 
 
 def audio_player(wf, sr):
@@ -50,8 +59,8 @@ def render_channel(channel_index, wf, sr):
     box = st.empty()
     with box.container():
         st.markdown(f'## channel-{channel_index}')
-        line_plot(wf, 'Waveform')
-        spectrum_plot(wf, 'Spectrum')
+        plot_data('Waveform', wf, sr, line_plot)
+        plot_data('Spectrum', wf, sr, spectrum_plot)
         audio_player(wf, sr)
 
 
