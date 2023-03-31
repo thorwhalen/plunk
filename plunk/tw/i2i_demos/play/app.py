1cb20779269d8f71c7722a19565ec9e3cf2ce5d2
Thor Whalen <thorwhalen1@gmail.com>
2023-03-31 18:24:49 +0200

    child stories POC -- debugging html rendering

--- plunk/tw/i2i_demos/play/app.py
+++ plunk/tw/i2i_demos/play/app.py
@@ -1,8 +1,9 @@
 from resources import funcs
-
 from extrude import mk_web_app
 
+from configurations import config
 
 if __name__ == '__main__':
-    app = mk_web_app(funcs, api_url='http://localhost:3030/openapi')
+    
+    app = mk_web_app(funcs, config=config, api_url='http://localhost:3030/openapi')
     app()
---
+++ plunk/tw/i2i_demos/play/configurations.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from dataclasses import dataclass
+from front.elements import OutputBase
+
+configs = None
+
+
+
+@dataclass
+class Markdown(OutputBase):
+    def render(self):
+        return st.markdown(
+            f"<html><body><h1>{self.output}</h1></body></html>",
+            unsafe_allow_html=True
+        )
+
+
+from front import APP_KEY, RENDERING_KEY, ELEMENT_KEY, NAME_KEY
+
+
+config = {
+    APP_KEY: {'title': "Making children's stories"},
+    RENDERING_KEY: {
+        'aggregate_story_and_image': {
+            NAME_KEY: 'Hit the value',
+            'description': {
+                'content': '''
+                    No description
+                ''',
+            },
+            'execution': {
+                'output': {
+                    ELEMENT_KEY: Markdown,
+                },
+            },
+        }
+    }
+}
