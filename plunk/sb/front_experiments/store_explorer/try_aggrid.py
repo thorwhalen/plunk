45f28d58e3a4427d876557ef70c76452d07e6c56
sylvainbonnot <sppbonnot@gmail.com>
2023-04-04 19:08:07 +0200

    update

---
+++ plunk/sb/front_experiments/store_explorer/try_aggrid.py
@@ -0,0 +1,25 @@
+from st_aggrid import AgGrid
+import pandas as pd
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
+import streamlit as st
+
+# get some data
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv"
+)
+# configure the grid to have selectable rows
+gb = GridOptionsBuilder.from_dataframe(df)
+gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+gridOptions = gb.build()
+
+
+data = AgGrid(
+    df,
+    gridOptions=gridOptions,
+    update_mode=GridUpdateMode.SELECTION_CHANGED,
+    enable_enterprise_modules=True,
+)
+
+# write the selected rows
+st.write(data["selected_rows"])
