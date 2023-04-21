cc566e508f4d38ccd9941eac9ec9cd26acc2d6d2
Andie.Phan <andie.phan@analog.com>
2023-04-21 14:07:11 -0700

    package dpp with pickle test

--- plunk/ap/package_dpp/pack.py
+++ plunk/ap/package_dpp/pack.py
@@ -1,16 +1,18 @@
 from pathlib import Path
-from isee.package_module import generate_package
+from wads.package_module import generate_package
 
 plunk_root = Path(__file__).parent.parent.parent
 
 if __name__ == '__main__':
     generate_package(
-        module_path=plunk_root / 'sb/front_demo/user_story1/simple_dpp_sushma.py',
+        module_path=plunk_root / 'ap/package_dpp/test_dpp',
         install_requires=[
             'olab @ git+ssh://git@github.com/otosense/olab.git',
             'PySoundFile',
             'numpy',
             'meshed',
         ],
-        output_path=plunk_root / 'ap/package_dpp/test_package',
+        output_path=plunk_root / 'ap/package_dpp/test_dpp_package',
+        glob_pattern='*.pkl',
+        version='1.0.0',
     )
