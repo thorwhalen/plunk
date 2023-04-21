951f99d27dd25a9f9789460e89724f6918fd310e
sylvainbonnot <sppbonnot@gmail.com>
2023-04-21 19:05:29 +0200

    update

--- plunk/sb/front_demo/user_story1/apps/app_illustration_extrude2/resources.py
+++ plunk/sb/front_demo/user_story1/apps/app_illustration_extrude2/resources.py
@@ -2,6 +2,7 @@ from meshed import code_to_dag
 from front.crude import prepare_for_crude_dispatch
 from functools import partial
 from mall import mall
+from front.dag import crudify_func_nodes
 
 
 @code_to_dag
@@ -16,35 +17,48 @@ funcs = list(dag.find_funcs())
 
 # ------------------------------------------------------------------------------
 # Adding persistence
+# q, f, g, r = funcs
+
+# crudifier = partial(
+#     prepare_for_crude_dispatch, mall=mall, include_stores_attribute=True
+# )
 
 
-q, f, g, r = funcs
+# def f(talking_point):
+#     return f"image of {talking_point}"
 
 
-crudifier = partial(
-    prepare_for_crude_dispatch, mall=mall, include_stores_attribute=True
+# def g(illustration_description):
+#     return illustration_description.upper()
+
+
+# ff = crudifier(
+#     output_store='illustration_description_store', output_trans=lambda: None
+# )(f)
+# gg = crudifier(
+#     param_to_mall_map={'illustration_description': 'illustration_description_store'},
+#     output_trans=lambda: None,
+# )(g)
+
+
+# funcs = [ff, gg, debug]
+
+# ------------------------------------------------------------------------------
+# crudifying the dag
+
+
+cdag = crudify_func_nodes(
+    'illustration_description', dag, mall=mall, include_stores_attribute=True
 )
+crudified_funcs = list(dag.find_funcs())
 
-
-def f(talking_point):
-    return f"image of {talking_point}"
-
-
-def g(illustration_description):
-    return illustration_description.upper()
-
-
-ff = crudifier(
-    output_store='illustration_description_store', output_trans=lambda: None
-)(f)
-gg = crudifier(
-    param_to_mall_map={'illustration_description': 'illustration_description_store'},
-    output_trans=lambda: None,
-)(g)
+# ------------------------------------------------------------------------------
+# Adding persistence
 
 
 def debug(x):
     return mall
 
 
-funcs = [ff, gg, debug]
+q, f, g, r = crudified_funcs
+funcs = [f, g, debug]
