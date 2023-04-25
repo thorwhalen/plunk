59d905af908622e3339127fef193e36f0e9f2aac
Thor Whalen <thorwhalen1@gmail.com>
2023-04-25 08:40:19 -0400

    chore: more experimentations in front and crudification

--- plunk/tw/i2i_demos/play/configurations.py
+++ plunk/tw/i2i_demos/play/configurations.py
@@ -1,25 +1,21 @@
 import streamlitfront.tools as t
 
-config = {}
+from front import APP_KEY
 
-from front import APP_KEY, RENDERING_KEY, NAME_KEY, ELEMENT_KEY
-from streamlitfront.elements import SelectBox
+config = {APP_KEY: {'title': 'Illustrating concepts'}}
 
-# config = {APP_KEY: {'title': 'Illustrating concepts'}}
+render_image_url = t.Pipe(
+    t.html_img_wrap, t.render_html,
+)
+render_bullet = t.Pipe(
+    t.lines_to_html_paragraphs, t.text_to_html, t.render_html,
+)
 
-# render_image_url = t.Pipe(
-#     t.html_img_wrap, t.render_html,
-# )
-# render_bullet = t.Pipe(
-#     t.lines_to_html_paragraphs, t.text_to_html, t.render_html,
-# )
-#
-# from front import APP_KEY
-# from streamlitfront.tools import trans_output, dynamic_trans
-#
-#
-# # THIS WORKS:
-# config = {APP_KEY: {'title': 'Illustrating concepts'}}
-# trans_output(config, 'topic_points', render_bullet)
-# trans_output(config, 'get_illustration', render_image_url)
-# trans_output(config, 'aggregate_story_and_image', t.dynamic_trans)
+from front import APP_KEY
+from streamlitfront.tools import trans_output, dynamic_trans
+
+# THIS WORKS:
+config = {APP_KEY: {'title': 'Illustrating concepts'}}
+trans_output(config, 'topic_points', render_bullet)
+trans_output(config, 'get_illustration', render_image_url)
+trans_output(config, 'aggregate_story_and_image', t.dynamic_trans)
--- plunk/tw/i2i_demos/play/resources.py
+++ plunk/tw/i2i_demos/play/resources.py
@@ -1,20 +1,14 @@
 from meshed import code_to_dag
 
-# import oa.examples.illustrate_stories as ii
+import oa.examples.illustrate_stories as ii  # https://pypi.org/project/oa/
 
-# func_src = {
-#     'get_talking_points': ii.topic_points,
-#     'make_illustration_description': ii.get_image_description,
-#     'make_illustration': ii.get_illustration,
-#     'aggregate_story_and_image': ii.aggregate_story_and_image,
-# }
-#
-#
-# @code_to_dag(
-#     func_src=func_src,
-#     name='slide_maker'
-# )
-@code_to_dag
+get_talking_points = ii.topic_points
+make_illustration_description = ii.get_image_description
+make_illustration = ii.get_illustration
+aggregate_story_and_image = ii.aggregate_story_and_image
+
+
+@code_to_dag(func_src=locals(), name='slide_maker')
 def dag():
     talking_points = get_talking_points(topic, n_talking_points)
     illustration_description = make_illustration_description(talking_point, image_style)
@@ -23,68 +17,3 @@ def dag():
 
 
 funcs = list(dag.find_funcs())
-
-# ------------------------------------------------------------------------------
-# Adding persistence
-
-
-q, f, g, r = funcs
-
-from front.crude import prepare_for_crude_dispatch, Crudifier
-from functools import partial
-
-
-mall = {'illustration_description_store': dict()}
-crudifier = partial(Crudifier, mall=mall)
-
-
-def f(talking_point):
-    return f"image of {talking_point}"
-
-
-def g(illustration_description):
-    return illustration_description.upper()
-
-
-ff = crudifier(
-    output_store='illustration_description_store',
-)(f)
-gg = crudifier(
-    param_to_mall_map={'illustration_description': 'illustration_description_store'},
-)(g)
-
-
-# ff = prepare_for_crude_dispatch(
-#     f,
-#     mall=mall,
-#     output_store='illustration_description_store',
-# )
-# gg = prepare_for_crude_dispatch(
-#     g,
-#     mall=mall,
-#     param_to_mall_map={'illustration_description': 'illustration_description_store'}
-# )
-
-# import streamlit as st
-
-
-def debug(x):
-    return mall
-
-
-funcs = [ff, gg, debug]
-
-# from front.dag import crudify_func_nodes
-#
-# cdag = crudify_func_nodes(
-#     'illustration_description', dag
-# )
-# funcs = list(cdag.find_funcs())
-
-import i2
-# print(*map(lambda x: f"{x.__name__}: {i2.Sig(x)}", funcs), sep='\n')
-#
-# print('-----------------------------------------------')
-# _, f, g, _ = list(cdag.find_funcs())
-print(f"{ff.__name__}: {i2.Sig(ff)}")
-print(f"{gg.__name__}: {i2.Sig(gg)}")
